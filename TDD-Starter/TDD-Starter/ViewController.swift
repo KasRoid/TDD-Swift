09233d4f1f017200db87f0cb54c2b9615a2c7975
KasRoid <KasRoid@gmail.com>
2021-01-02 09:34:57 +0900

    UI: Basic Setup

--- TDD-Starter/TDD-Starter/ViewController.swift
+++ TDD-Starter/TDD-Starter/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - Properties
+    @IBOutlet weak var textField1: UITextField!
+    @IBOutlet weak var textField2: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
+    
+    // MARK: - Selectors
+    @IBAction func handleButton(_ sender: UIButton) {
+        
+    }
 }
 
